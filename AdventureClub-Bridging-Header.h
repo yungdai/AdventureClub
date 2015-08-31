d60f9d79001d1608cf709a36ee857d52d717dc24
Yung Dai <yungchidai@gmail.com>
2015-08-31 09:49:27 -0400

    synch for later

---
+++ AdventureClub-Bridging-Header.h
@@ -0,0 +1,24 @@
+//
+//  AdventureClub-Bridging-Header.h
+//  AdventureClub
+//
+//  Created by Yung Dai on 2015-08-29.
+//  Copyright © 2015 Yung Dai. All rights reserved.
+//
+
+#ifndef AdventureClub_Bridging_Header_h
+#define AdventureClub_Bridging_Header_h
+
+
+#endif /* AdventureClub_Bridging_Header_h */
+// Parse API
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+#import <Bolts/Bolts.h>
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+
+
+// Facebook API
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
